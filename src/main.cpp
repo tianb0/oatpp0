a5e5af6eb1d43d0bac5f94ffacd07664be56f9b8
Tianbo <tianbo@hey.com>
2023-03-26 18:22:32 -0700

    get route

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,27 @@
+#include <memory>
 #include <oatpp/core/base/Environment.hpp>
 #include <oatpp/web/server/HttpConnectionHandler.hpp>
 
 #include <oatpp/network/Server.hpp>
 #include <oatpp/network/tcp/server/ConnectionProvider.hpp>
 
+// request handler
+class Handler : public oatpp::web::server::HttpRequestHandler {
+public:
+
+  // handle incoming request and return outgoing response
+  std::shared_ptr<OutgoingResponse> handle(const std::shared_ptr<IncomingRequest>& request) override {
+    return ResponseFactory::createResponse(Status::CODE_200, "hello world");
+  }
+};
+
 void run() {
 
   // create router for http requests routing
   auto router = oatpp::web::server::HttpRouter::createShared();
+
+  // route GET /hello
+  router->route("GET", "/hello", std::make_shared<Handler>());
 
   // create http  connection handler with router
   auto connectionHandler = oatpp::web::server::HttpConnectionHandler::createShared(router);
